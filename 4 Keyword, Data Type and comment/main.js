271c533e333ccab57070038d9ef459c9d7f924ea
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-04-15 11:23:01 +0600

    typeof use and comment

---
+++ 4 Keyword, Data Type and comment/main.js
@@ -0,0 +1,11 @@
+var num = typeof(123);
+document.write(num); //number data
+
+var num = typeof("Md. Ehasan Ali");
+document.write(num); //string data
+
+var num = typeof("");
+document.write(num); //empty data
+
+var num = typeof(true);
+document.write(num); // boolean data
