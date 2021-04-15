07ea0166013e7e81d14a1887866adc46033ed2eb
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-04-15 12:26:30 +0600

    variable keyword use korar all niyom

---
+++ 4 Variables/main.js
@@ -0,0 +1,31 @@
+// ata akvabe likha jai
+var nam = "Md. Ehasan Ali <br/>";
+
+document.write(nam);
+
+const name = "Phone Number: 01752128520 <br/>"
+
+document.write(name);
+
+
+// variable ata ko onnovabe likha jai
+
+var myName;
+var age;
+
+myName = "Md. Ehasan Ali";
+age = " 18";
+
+document.write(myName + age + "<br/>");
+
+
+
+
+// comma use kore onek gula variable add kora jaii .
+
+var myName, age;
+
+myName = "Md. Ehasan Ali"
+age = " 18"
+
+document.write(myName + age)
