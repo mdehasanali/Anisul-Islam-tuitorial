4a0c381b74f55272061d253ce442694a553e784c
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-04-17 12:40:57 +0600

    main.js file chenge toString,tofixed, toPrecision,

---
+++ 5 number method - toFixed - toPrecision/main.js
@@ -0,0 +1,46 @@
+/*
+var num = 20;
+console.log(typeof(num)); // data ki type seta dekhabe
+*/
+
+/*
+var num = 20;
+var num = toString(20);
+console.log(typeof(num)); // number k string a rupantorito korbe
+*/
+
+
+/*
+var num = 20.55;
+var purnoShonkha = parseInt(num);
+console.log(purnoShonkha); // purnoShonkha show korbe 
+*/
+
+/*
+var num = 20.55;
+var dosomikSongkha = parseFloat(num);
+console.log(dosomikSongkha); // dosomikSongkha show korbe
+*/
+
+
+/*
+var number = 20.87956957;
+console.log(number.toFixed(5)); // toFixed er value joto dibo toita dosomik por toyta Songkhar dekhabe
+*/
+
+/*
+var number = 20.87956957;
+console.log(number.toPrecision(5)); // toPrecision er value joto dibo toita dosomik soho toyta Songkhar dekhabe
+*/
+
+
+/*
+console.log(typeof(Number(12)));
+console.log(Number("    12   "));
+console.log(typeof(Number(12.8)));
+console.log(Number("   12.8   "));
+
+console.log(Number(true)); // er man 1
+console.log(Number(false)); // er man 0
+
+*/
