e5cc5247272d867732cafcca4f6be1b64698ca90
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-04-19 10:06:00 +0600

    9 calculetor create prompt used in main.js file

--- 9 Make your own calculator/main.js
+++ 9 Make your own calculator/main.js
@@ -11,3 +11,30 @@ document.write("sub = " + result + "<br/ >"); // (-) create kor
 var result = number1 * number2;
 document.write("sub = " + result + "<br/ >"); // (*) create kor
 */
+
+
+// ইউজার জেতা টাইপ করবে সেটা string আকারে আসবে ।
+// এই জন্য string কে parseInt এ রুপান্তরিত করা । তাহলে string টি Number এ পরিনত হবে ।
+
+/*
+var number1 = prompt("Enter First Number : ");
+var number2 = prompt("Enter Second Number : ");
+
+var number1 = parseInt(number1, 10);
+var number2 = parseInt(number2, 10);
+
+var sum = number1 + number2;
+document.write(number1 + " + " + number2 + " = " + sum + "<br/>"); // jog ko ra
+
+var sub = number1 - number2;
+document.write(number1 + " - " + number2 + " = " + sub + "<br/>"); // beyog ko ra
+
+var intu = number1 * number2;
+document.write(number1 + " * " + number2 + " = " + intu + "<br/>"); // gun ko ra
+
+var vag = number1 / number2;
+document.write(number1 + " / " + number2 + " = " + vag + "<br/>"); // vag ko ra
+
+var vag_fol = number1 % number2;
+document.write(number1 + " % " + number2 + " = " + vag_fol + "<br/>"); // vag_fol ko ra
+*/
