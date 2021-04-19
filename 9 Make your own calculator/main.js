3ca70c0266df48cad5471eb91c8321522e3b9369
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-04-19 09:38:41 +0600

    9 main.js file chenge

---
+++ 9 Make your own calculator/main.js
@@ -0,0 +1,13 @@
+/*
+var number1 = 50;
+var number2 = 20;
+
+var result = number1 + number2;
+document.write("sum = " + result + "<br/ >"); // (+) create kor
+
+var result = number1 - number2;
+document.write("sub = " + result + "<br/ >"); // (-) create kor
+
+var result = number1 * number2;
+document.write("sub = " + result + "<br/ >"); // (*) create kor
+*/
