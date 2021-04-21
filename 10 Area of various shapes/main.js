ae880ea273c6372eb1b50510a4f5cd30e4423478
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-04-21 10:13:55 +0600

    Square, Rectangle, Traingle, function added

---
+++ 10 Area of various shapes/main.js
@@ -0,0 +1,29 @@
+// square = borgo
+// Rectangle = Aioto
+// Triangle = treevuj
+
+
+
+// var base =  parseFloat(prompt("Enter Base"));
+// var height = parseFloat(prompt("Enter Height"));
+
+// var square = base * height;
+// document.write("square " + square);
+
+
+
+
+// var base =  parseFloat(prompt("Enter Base"));
+// var height = parseFloat(prompt("Enter Height"));
+
+// var Rectangle = base * height;
+// document.write("Rectangle " + Rectangle);
+
+
+
+
+var base = parseFloat(prompt("Enter Base"));
+var height = parseFloat(prompt("Enter Height"));
+
+var Triangle = (base * height) / 2;
+document.write("Triangle " + Triangle);
