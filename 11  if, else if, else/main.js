bd2928caaabbaadceadfb9b9fef80363d3f68477
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-04-22 10:38:46 +0600

    html file sctipt tag mve & main.js file paste here

---
+++ 11  if, else if, else/main.js
@@ -0,0 +1,40 @@
+/*
+// if, else used
+var num = 5;
+if (num % 2 == 0) {
+    document.write("Even");
+} else {
+    document.write("Odd");
+*/
+
+
+/*
+// if, elseif, else used by script
+var num = prompt("Enter Your Number : ");
+if (num > 0) {
+    document.write("Positive");
+} else if (num < 0) {
+    document.write("Negative");
+} else {
+    document.write("Zero");
+}
+*/
+
+
+
+var marks = prompt("Enter Your Marks Number : ");
+if (marks >= 80) {
+    document.write("A+");
+} else if (marks >= 70) {
+    document.write("A");
+} else if (marks >= 60) {
+    document.write("A-");
+} else if (marks >= 50) {
+    document.write("B");
+} else if (marks >= 40) {
+    document.write("C");
+} else if (marks >= 33) {
+    document.write("D");
+} else {
+    document.write("Fail")
+}
