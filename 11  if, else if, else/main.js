0684d863432ec0870bde538c5663acd1a66a3694
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-04-22 15:08:30 +0600

    programs using logical operators added program

--- 11  if, else if, else/main.js
+++ 11  if, else if, else/main.js
@@ -45,17 +45,19 @@ if (marks >= 80) {
 
 // Letter grade Programs using logical operators
 var marks = prompt("Enter Your Marks Number : ");
-if (marks >= 80) {
+if (marks > 100 || marks < 0) {
+    document.write("Invalid marks");
+} else if (marks >= 80 && marks <= 100) {
     document.write("A+");
-} else if (marks >= 70) {
+} else if (marks >= 70 && marks <= 79) {
     document.write("A");
-} else if (marks >= 60) {
+} else if (marks >= 60 && marks <= 69) {
     document.write("A-");
-} else if (marks >= 50) {
+} else if (marks >= 50 && marks <= 59) {
     document.write("B");
-} else if (marks >= 40) {
+} else if (marks >= 40 && marks <= 49) {
     document.write("C");
-} else if (marks >= 33) {
+} else if (marks >= 33 && marks <= 39) {
     document.write("D");
 } else {
     document.write("Fail")
