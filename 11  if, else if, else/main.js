d045ac9d96852eb53c773ac621984fade2100276
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-04-23 10:34:40 +0600

    digt spelling with switch, case, breck and default

--- 11  if, else if, else/main.js
+++ 11  if, else if, else/main.js
@@ -96,7 +96,7 @@ if (letters == "A" || letters == "E" || letters == "I" || letters == "O" || lett
 
 
 
-
+/*
 // digit spelling
 // 0 - Zero , 1 - one ..... 9 -nine, ten - not a valid digit
 var digit = prompt("Enter One Digit Number :- ");
@@ -123,3 +123,47 @@ if (digit == 0) {
 } else {
     document.write("Not a valid digit");
 }
+*/
+
+
+
+/*
+// digit spelling with switch programing 
+// 0 - Zero , 1 - one ..... 9 -nine, ten - not a valid digit
+var digit = prompt("Enter One Digit Number :- ");
+switch (digit) {
+    case "0":
+        document.write("Zero");
+        break;
+    case "1":
+        document.write("One");
+        break;
+    case "2":
+        document.write("Two");
+        break;
+    case "3":
+        document.write("Three");
+        break;
+    case "4":
+        document.write("Four");
+        break;
+    case "5":
+        document.write("Five");
+        break;
+    case "6":
+        document.write("Six");
+        break;
+    case "7":
+        document.write("Seven");
+        break;
+    case "8":
+        document.write("Eight");
+        break;
+    case "9":
+        document.write("Nine");
+        break;
+
+    default:
+        document.write("Not a valid digit");
+}
+*/
