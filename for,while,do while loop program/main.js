1a0905ba67e67e3678b404020571810d98e5154a
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-04-24 11:35:23 +0600

    1 theke 100 number show

--- for,while,do while loop program/main.js
+++ for,while,do while loop program/main.js
@@ -7,6 +7,17 @@ for (var x = 1; x <= 10; x = x + 1) {
 
 
 /*
+// 1 theke 100 number show
+for (
+    var num = 1; num <= 100; num = num + 1
+) {
+    document.write(num + " <br/>");
+}
+*/
+
+
+
+/*
 // odd number show 
 // 1 3 5 7 9 11 13 ...... 99
 for (var x = 1; x <= 99; x = x + 2) {
@@ -15,8 +26,11 @@ for (var x = 1; x <= 99; x = x + 2) {
 */
 
 
+
+/*
 // Even number show 
 // 2 4 6 8 10 12 14 ..... 100 
 for (var x = 0; x <= 100; x = x + 2) {
     document.write(" " + x);
 }
+*/
