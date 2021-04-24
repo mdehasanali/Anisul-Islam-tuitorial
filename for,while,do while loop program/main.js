b2732eeb00a0d1e6f667e637669fb192c5065f13
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-04-24 11:29:36 +0600

    Even number show  2 4 6 8 10

--- for,while,do while loop program/main.js
+++ for,while,do while loop program/main.js
@@ -6,9 +6,17 @@ for (var x = 1; x <= 10; x = x + 1) {
 
 
 
-
+/*
 // odd number show 
 // 1 3 5 7 9 11 13 ...... 99
 for (var x = 1; x <= 99; x = x + 2) {
+    document.write(" " + x);
+}
+*/
+
+
+// Even number show 
+// 2 4 6 8 10 12 14 ..... 100 
+for (var x = 0; x <= 100; x = x + 2) {
     document.write(" " + x);
 }
