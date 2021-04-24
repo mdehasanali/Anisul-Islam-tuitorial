dd3bd2f8a69d68aa6c2408c263a6d2b75d257285
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-04-24 11:26:38 +0600

    odd number show  1 3 5 7 9

--- for,while,do while loop program/main.js
+++ for,while,do while loop program/main.js
@@ -1,3 +1,14 @@
-// for (var x = 1; x <= 10; x = x + 1) {
-//     document.write("Bangladesh " + x + " <br/>");
-// }
+/*
+for (var x = 1; x <= 10; x = x + 1) {
+    document.write("Bangladesh " + x + " <br/>");
+}
+*/
+
+
+
+
+// odd number show 
+// 1 3 5 7 9 11 13 ...... 99
+for (var x = 1; x <= 99; x = x + 2) {
+    document.write(" " + x);
+}
