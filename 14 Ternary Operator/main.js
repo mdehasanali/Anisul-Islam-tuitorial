b53f379dbe8dbb1e7639a3c8451df96d80e79c34
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-04-26 16:05:01 +0600

    14 Ternary Operator program added in main.js file

---
+++ 14 Ternary Operator/main.js
@@ -0,0 +1,34 @@
+/*
+var num = Number(prompt("Enter Your Number :- "));
+if (num >= 0) {
+    document.write("Positive");
+} else {
+    document.write("Negative");
+}
+
+// var result = num >= 0 ? "Positive" : "Negative";
+// document.write(result);
+
+//var result = num >= 0 ? document.write("Positive") : document.write("Negative");
+*/
+
+
+
+
+
+
+
+
+var num = Number(prompt("Enter Your Number :- "));
+// if (num > 0) {
+//     document.write("Positive");
+// } else if (num < 0) {
+//     document.write("Negative");
+// } else {
+//     document.write("Zero");
+// }
+
+// var result = num > 0 ? "Positive" : num < 0 ? "Negative" : "Zero";
+// document.write(result);
+
+var result = num > 0 ? document.write("Positive") : num < 0 ? document.write("Negative") : document.write("Zero");
