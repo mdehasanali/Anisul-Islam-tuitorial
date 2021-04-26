d5ab42d7e912528e2d458cd27f20c07097ee439a
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-04-26 12:16:02 +0600

    break and continue er babohar

---
+++ 13 break and continue/main.js
@@ -0,0 +1,70 @@
+// break অর্থ ভেঙ্গে দেওয়া
+// 
+
+
+
+/*
+for (var i = 1; i <= 100; i++) {
+    document.write(" " + i);
+}
+*/
+
+
+
+/*
+// break function dia loop ba content break ko ra hoi
+for (var i = 1; i <= 100; i++) {
+    if (i == 10) {
+        break
+    }
+    document.write(" " + i);
+}
+*/
+
+
+
+/*
+for (var i = 1; i <= 100; i++) {
+    document.write(" " + i);
+    if (i == 10) {
+        break
+    }
+}
+*/
+
+
+
+/*
+// continue
+// 10 bad dia bake sob shoe ho be
+for (var i = 1; i <= 100; i++) {
+    if (i == 10) {
+        continue
+    }
+    document.write(" " + i);
+}
+*/
+
+
+
+/*
+// jor gula k bad dia continue kor be
+for (var i = 1; i <= 100; i++) {
+    if (i % 2 == 0) {
+        continue
+    }
+    document.write(" " + i);
+}
+*/
+
+
+
+/*
+// বিজোড় gula k bad dia continue kor be
+for (var i = 1; i <= 100; i++) {
+    if (i % 2 != 0) {
+        continue
+    }
+    document.write(" " + i);
+}
+*/
