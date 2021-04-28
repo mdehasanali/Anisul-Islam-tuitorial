1193ebfcae18c37d08a3a65300b4413250682977
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-04-28 09:38:14 +0600

    jog and biyog kora

---
+++ 15 Function/main.js
@@ -0,0 +1,23 @@
+/*
+// (+) function added
+function sup() {
+    var num1 = parseInt(prompt("Enter Your First Number :- "));
+    var num2 = parseInt(prompt("Enter Your Second Number :- "));
+    var result = num1 + num2;
+    document.write("Result = " + result);
+}
+sup();
+*/
+
+
+
+/*
+// (-) function added
+function sub() {
+    var num1 = parseInt(prompt("Enter Your First Number :- "));
+    var num2 = parseInt(prompt("Enter Your Second Number :- "));
+    var result = num1 - num2;
+    document.write("Result = " + result);
+}
+sub();
+*/
