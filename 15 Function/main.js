8019c90b5ac993b5bf33d5f659ea00b57b6b5036
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-04-28 09:42:18 +0600

    simple variable added

--- 15 Function/main.js
+++ 15 Function/main.js
@@ -1,10 +1,28 @@
 /*
+var number = 8;
+var result = number * number;
+document.write("Result = " + result + "<br/>");
+
+var number = 7;
+var result = number * number;
+document.write("Result = " + result + "<br/>");
+
+var number = 9;
+var result = number * number;
+document.write("Result = " + result + "<br/>");
+*/
+
+
+
+
+
+/*
 // (+) function added
 function sup() {
     var num1 = parseInt(prompt("Enter Your First Number :- "));
     var num2 = parseInt(prompt("Enter Your Second Number :- "));
     var result = num1 + num2;
-    document.write("Result = " + result);
+    document.write("Result = " + result + "<br/>");
 }
 sup();
 */
@@ -17,7 +35,7 @@ function sub() {
     var num1 = parseInt(prompt("Enter Your First Number :- "));
     var num2 = parseInt(prompt("Enter Your Second Number :- "));
     var result = num1 - num2;
-    document.write("Result = " + result);
+    document.write("Result = " + result + "<br/>");
 }
 sub();
 */
