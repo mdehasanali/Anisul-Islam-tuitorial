f95451e8b547f5b4a246a5fc02c2ac9f25289555
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-05-01 11:05:36 +0600

    simple object added

---
+++ 17 object/main.js
@@ -0,0 +1,75 @@
+/*
+var name = "Md. Ehasan Ali";
+var age = 18;
+var CGPA = 2.81;
+var Phone_number = 8801752128520;
+var language = ["Bangla", "Hindi", "English"];
+
+console.log(name);
+console.log(age);
+console.log(CGPA);
+console.log(Phone_number);
+console.log(language);
+*/
+
+/*
+var ditails = {
+    name: "MD. Ehasan Ali",
+    age: 18,
+    CGPA: 2.81,
+    Phone_number: 8801752128520,
+    language: ["Bangla", "Hindi", "English"]
+}
+console.log(ditails.name);
+console.log(ditails.age);
+console.log(ditails.CGPA);
+console.log(ditails.Phone_number);
+console.log(ditails.language);
+*/
+
+
+
+
+/*
+var ditails1 = {
+    name: "MD. Ehasan Ali",
+    age: 18,
+    CGPA: 3.81,
+    Phone_number: 8801752128520,
+    language: ["Bangla", "Hindi", "English"]
+}
+
+var ditails2 = {
+    name: "Md Sanaullah",
+    age: 19,
+    CGPA: 3.41,
+    Phone_number: 8801752128520,
+    language: ["Bangla", "English", "Japanis"]
+}
+
+var ditails3 = {
+    name: "Mst Sahida Khatun",
+    age: 16,
+    CGPA: 2.81,
+    Phone_number: 8801870157618,
+    language: ["Bangla", "Farsi", "Chinis"]
+}
+
+console.log(ditails1.name);
+console.log(ditails1.age);
+console.log(ditails1.CGPA);
+console.log(ditails1.Phone_number);
+console.log(ditails1.language);
+
+console.log(ditails2.name);
+console.log(ditails2.age);
+console.log(ditails2.CGPA);
+console.log(ditails2.Phone_number);
+console.log(ditails2.language);
+
+console.log(ditails3.name);
+console.log(ditails3.age);
+console.log(ditails3.CGPA);
+console.log(ditails3.Phone_number);
+console.log(ditails3.language);
+*/
