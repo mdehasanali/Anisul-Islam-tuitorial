2343b12989c5a6d54289f733ca910569d01206c3
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-05-02 10:18:47 +0600

    math objcet all methor and propaty

---
+++ 17 object/math_object.js
@@ -0,0 +1,65 @@
+// sqrt dia borgo kora hoi
+console.log(Math.sqrt(49));
+
+
+// (+) kinba (-) number e hok (+) number a rupantorito korbe 
+console.log(Math.abs(-7));
+
+
+// ত্রিকোণমিতি function 
+console.log(Math.sin(30));
+console.log(Math.cos(60));
+console.log(Math.tan(45));
+
+
+// x er power holo y 
+console.log(Math.pow(2, 4));
+
+
+// floor function dia nicher pronosongka ; 2.8 = 2 hobe
+console.log(Math.floor(2.3));
+console.log(Math.floor(2.8));
+
+
+// ceil function dia nicher pronosongka ; 2.4 = 3 hobe
+console.log(Math.ceil(2.4));
+console.log(Math.ceil(2.8));
+
+
+// round kore j dike hobe sai dike
+console.log(Math.round(2.4));
+console.log(Math.round(2.8));
+
+
+/*
+// max a sorbocco  number ber korbe
+// min a sorbonimno number ber korbe
+console.log(Math.max(8, 45, -54, 51));
+console.log(Math.min(8, 45, -54, 51));
+
+var num1 = parseInt(prompt("Enter Your First Number"));
+var num2 = parseInt(prompt("Enter Your sceond Number"));
+var num3 = parseInt(prompt("Enter Your three Number"));
+console.log(Math.max(num1, num2, num3));
+console.log(Math.min(num1, num2, num3));
+*/
+
+
+
+
+// math rendom
+// 0 theke besi hote parbe nahh
+console.log(Math.random());
+
+// * 5 dile 0 theke 6 er besihobe nahh = 0,1,2,3,4,5
+console.log(Math.random() * 6);
+
+// purnosongshar random number
+console.log(Math.floor(Math.random() * 6));
+
+// bortoman a ache 0 theke 10 ;
+// ami chai 1 theke 10
+console.log(Math.floor(Math.random() * 10) + 1);
+
+// ami chai 10 theke 20
+console.log(Math.floor(Math.random() * 11) + 10);
