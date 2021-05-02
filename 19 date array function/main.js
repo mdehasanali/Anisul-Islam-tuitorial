069c1bafe02432cc839f981e24597e6caab9d10a
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-05-02 16:29:19 +0600

    date methor added

---
+++ 19 date array function/main.js
@@ -0,0 +1,28 @@
+var date = new Date();
+
+// full history show korbe
+console.log(date);
+
+// tarik show korbe
+console.log(date.getDate());
+
+// bar show korbe
+console.log(date.getDay());
+
+// year show korbe
+console.log(date.getFullYear());
+
+// hour show korbe
+console.log(date.getHours());
+
+// milisecond show korbe
+console.log(date.getMilliseconds());
+
+// minute show korbe
+console.log(date.getMinutes());
+
+// second show korbe
+console.log(date.getSeconds());
+
+// hour show korbe
+console.log(date.getTime());
