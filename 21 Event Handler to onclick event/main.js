b5628e0e9122415b35661a5bcb1671d01c84d482
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-05-03 10:55:16 +0600

    btn & img tag add and onclick propary added

---
+++ 21 Event Handler to onclick event/main.js
@@ -0,0 +1,20 @@
+var query = document.querySelector("p");
+
+function mybtn1() {
+    query.innerHTML = "Hello ! I am a button 1";
+};
+
+function mybtn2() {
+    query.innerHTML = "Hello ! I am a button 2";
+};
+
+
+var imgQuery = document.querySelector(".img");
+
+function mypic1() {
+    imgQuery.src = "img/Ehasan.jpg"
+};
+
+function mypic2() {
+    imgQuery.src = "img/sanaullah.jpg"
+};
