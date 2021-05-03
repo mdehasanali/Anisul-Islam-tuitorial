f57035fda13ca263da4001426f1a6a38c947bde9
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-05-03 10:16:15 +0600

    dom fldr ad and id class tag query html selector

---
+++ 20 Introduction to DOM/main.js
@@ -0,0 +1,16 @@
+document.getElementById("id").innerHTML = "Hello ! Ehasan id";
+
+document.getElementsByClassName("class")[0].innerHTML = "Hello ! Ehasan Class 1";
+
+document.getElementsByClassName("class")[1].innerHTML = "Hello ! Ehasan Class 2";
+
+
+document.getElementsByTagName("h1")[0].innerHTML = "MD Ehasan Ali";
+
+
+
+
+// querySelector element
+document.querySelector("#q-id").innerHTML = "id q";
+document.querySelector(".q-class").innerHTML = "class q 1";
+document.querySelector("h5").innerHTML = "heading tag 5"
