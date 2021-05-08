3e416abe431af65810886cf289129d02b60c3512
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-05-08 12:10:31 +0600

    rename throw name change

---
+++ 29 Error Handling-- try, catch, finally block/throw.js
@@ -0,0 +1,24 @@
+// document.querySelector(".btn").addEventListener("click", function() {
+//     var text = document.querySelector(".textField").value;
+//     console.log(text);
+// });
+
+
+
+
+document.querySelector(".btn").addEventListener("click", function() {
+    var text = document.querySelector(".textField").value;
+
+    try {
+        if (text < 5) {
+            throw " input is too low"
+        } else if (text > 10) {
+            throw "input is too high"
+        } else(
+            console.log(text)
+        )
+    } catch (error) {
+        console.log(error);
+    }
+
+});
