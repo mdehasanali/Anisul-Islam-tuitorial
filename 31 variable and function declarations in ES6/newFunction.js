170cbe0669f9e263ac9ed49085439dbd11052e58
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-05-12 11:02:47 +0600

    new function added

---
+++ 31 variable and function declarations in ES6/newFunction.js
@@ -0,0 +1,17 @@
+/*
+// old function
+function add(x, y) {
+    var sum = x + y;
+    console.log(sum);
+}
+add(10, 40);
+*/
+
+
+
+// new function
+const add = (x, y) => {
+    let sum = x + y;
+    console.log(sum);
+}
+add(10, 40);
