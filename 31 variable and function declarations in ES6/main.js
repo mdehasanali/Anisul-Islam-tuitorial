b883d573f91eb77b31e1dec279644a4ec8600b4a
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-05-12 10:51:58 +0600

    ES6 javascript

---
+++ 31 variable and function declarations in ES6/main.js
@@ -0,0 +1,41 @@
+/*
+// ES5 old varson javaScript
+var x = 6;
+if (true) {
+    var x = 4
+}
+console.log(x);
+*/
+
+
+
+/*
+// ES6 new varson javaScript variable
+let y = 6;
+if (true) {
+    let y = 4
+}
+console.log(y);
+*/
+
+
+
+/*
+// ES6 new varson javaScript for loop
+let index = 11;
+for (let index = 0; index <= 10; index++) {
+    console.log(index);
+}
+console.log(index);
+*/
+
+
+
+
+/*
+//  variable man change hobe nah but variable likhar arek niyom
+const x = 12;
+if (true) {
+    x = 10
+}
+*/
