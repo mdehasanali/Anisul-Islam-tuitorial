5bacf30c9cfe506656eed65ad9262885892b7636
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-05-12 10:51:37 +0600

    new string concanetor

---
+++ 31 variable and function declarations in ES6/newStringConcat.js
@@ -0,0 +1,20 @@
+/*
+// old string concanetor
+var x = 24;
+var y = 51;
+var sum = x + y;
+console.log("Sum is " + sum + " the End");
+
+
+// new string concanetor
+let x = 50;
+let y = 41;
+let sum = x + y;
+console.log(`Sum is ${sum} the end`);
+
+
+// new string concanetor
+let name = `Md: Ehasan Ali`;
+let message = `I am ${name}. I am learing javaScript`
+console.log(message);
+*/
