c49a23216e14009091e77c705d1dad32c7c2984c
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-05-12 12:00:06 +0600

    hoisting and strict mode  added

---
+++ 32 hoisting and strict mode/main.js
@@ -0,0 +1,37 @@
+/*
+// variable pore ডিক্লার করলে ও হবে 
+// কিন্তু let and const পরে দিক্লার করলে হবে না।
+x = 20;
+console.log(x);
+var x;
+
+// Uncaught ReferenceError: show korbe
+y = 20;
+console.log(y);
+let y;
+
+// Uncaught SyntaxError: show korbe
+z = 20;
+console.log(z);
+const z;
+*/
+
+
+/*
+// var not diklar
+x = 20;
+if (true) {
+    y = 40;
+    console.log(`y = ${y}`);
+}
+console.log(`x = ${x}`);
+var x;
+*/
+
+
+/*
+//  variable diklar chara hobe result show hobe nahh
+"use strict"
+x = 20;
+console.log(`x = ${x}`);
+*/
