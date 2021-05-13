127520b1ed3b77e3962c4ebd26aae697727a7c16
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-05-13 11:31:20 +0600

    defults parametter pass user not input the value

---
+++ 33 default and rest parameter/main.js
@@ -0,0 +1,12 @@
+// function meassage(message) {
+//     console.log(`${message}`);
+// };
+// meassage("This is a test demo")
+
+
+
+function meassage(message = `defolts`) {
+    console.log(`${message}`);
+};
+meassage()
+meassage("This is a test demo")
