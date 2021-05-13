8d8f6bb49ebff31d89e801307f2f6a39c4e8ccfa
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-05-13 20:10:08 +0600

    rest parlament mathor added

--- 33 default and rest parameter/main.js
+++ 33 default and rest parameter/main.js
@@ -5,8 +5,24 @@
 
 
 
-function meassage(message = `defults`) {
-    console.log(`${message}`);
-};
-meassage()
-meassage("This is a test demo")
+// function meassage(message = `defults`) {
+//     console.log(`${message}`);
+// };
+// meassage()
+// meassage("This is a test demo")
+
+
+
+
+// rest parlament
+function message(x, y, ...z) {
+    console.log(x, y, ...z);
+}
+message(10, 211, 55, 21, 24)
+
+
+// rest parlament
+function message(x, y, ...z) {
+    console.log(`x = ${x} y = ${y} z = ${z}`);
+}
+message(10, 211, 55, 21, 24)
