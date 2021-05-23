c60e7c6f8b862e2b74caa8611d4e8ccb5f094b87
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-05-23 08:12:00 +0600

    map and filter finction added

---
+++ 12 for,while,do while loop program/map, filter function.js
@@ -0,0 +1,47 @@
+// map & filter function added
+
+/*
+let numbers = [10, 20, 30, 40, 50];
+let squar = [];
+numbers.forEach(function(x) {
+    squar.push(x * x);
+});
+console.log(numbers);
+console.log(squar);
+
+
+let numbers = [10, 20, 30, 40, 50];
+let squar = [];
+numbers.forEach(x => {
+    squar.push(x * x);
+});
+console.log(numbers);
+console.log(squar);
+
+
+
+
+
+// new array create hobe return variable
+// callback function  push mother korte hobe na 
+// akdom simple
+
+let numbers = [10, 20, 30, 40, 50];
+let squar = numbers.map(x => {
+    return x * x;
+});
+console.log(numbers);
+console.log(squar);
+*/
+
+
+
+
+
+
+// let numbers = [20, 5, 40, 9, 70, 1, 90, 40, 7, 54, 0, 99];
+// let newNumbers = numbers.filter(x => {
+//     return x > 10
+// });
+// console.log(newNumbers);
+// console.log(newNumbers.length);
