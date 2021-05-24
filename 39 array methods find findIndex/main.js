405d85d49f7ac671be13c03630e7d7a0a4c9c5c5
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-05-24 22:11:13 +0600

    array methods find() | findIndex() | added object methods find() | findIndex() | added

---
+++ 39 array methods find findIndex/main.js
@@ -0,0 +1,50 @@
+// find (callback, value )return the value of the first element that pass certain candotion
+
+
+// let numbers = [11, 41, 20, 45, 48, ];
+// let firstNumbers = numbers.find(x => x % 2 === 0)
+// console.log(firstNumbers);
+
+
+// let numbers = [11, 41, 20, 45, 48, ];
+// let firstNumbers = numbers.find((x) => {
+//     if (x % 2 === 0) {
+//         return x
+//     }
+// })
+// console.log(firstNumbers);
+
+
+
+
+
+
+
+
+
+
+// findIndex (callback, value )return the index of the first element that pass certain candotion
+
+
+// let numbers = [11, 41, 20, 45, 48, ];
+// let firstNumbers = numbers.findIndex(x => x % 2 === 0)
+// console.log(firstNumbers);
+
+
+// let numbers = [11, 41, 20, 45, 48, ];
+// let firstNumbers = numbers.findIndex((x) => {
+//     if (x % 2 === 0) {
+//         return x
+//     }
+// })
+// console.log(firstNumbers);
+
+
+// let numbers = [11, 41, 20, 45, 48, ];
+// const myFunction = (value, index, array) => {
+//     if (value % 2 === 0) {
+//         return value;
+//     }
+// }
+// let firstNumbers = numbers.findIndex(myFunction);
+// console.log(firstNumbers);
