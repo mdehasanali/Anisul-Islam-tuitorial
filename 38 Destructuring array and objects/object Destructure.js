10b513e0a118253540aeef50bb606efca2194f71
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-05-24 08:29:16 +0600

    swap variable file comment and object destructure program added

---
+++ 38 Destructuring array and objects/object Destructure.js
@@ -0,0 +1,32 @@
+// const studentInfo = {
+//     id: 101,
+//     fullName: 'Md. Ehasan Ali',
+//     age: 18,
+//     cgpa : 2.81
+// }
+
+// console.log(studentInfo.id);
+// console.log(studentInfo.fullName);
+// console.log(studentInfo.age);
+
+
+
+
+const studentInfo = {
+    id: 101,
+    fullName: 'Md. Ehasan Ali',
+    age: 18,
+    cgpa: 2.81
+};
+
+const {
+    id,
+    fullName,
+    age,
+    cgpa
+} = studentInfo;
+
+console.log(id);
+console.log(fullName);
+console.log(age);
+console.log(cgpa);
--- 38 Destructuring array and objects/swap Variable.js
+++ 38 Destructuring array and objects/swap Variable.js
@@ -1,5 +1,5 @@
-var num1 = 10,
-    num2 = 20;
-[num1, num2] = [num2, num1]
-console.log(num1);
-console.log(num2);
+// var num1 = 10,
+//     num2 = 20;
+// [num1, num2] = [num2, num1]
+// console.log(num1);
+// console.log(num2);
