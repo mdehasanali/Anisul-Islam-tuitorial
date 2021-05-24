83edbb303a573caf5fa817d35f005b6ca234f691
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-05-24 08:09:21 +0600

    array desturcture program added

---
+++ 38 Destructuring array and objects/array Destructure.js
@@ -0,0 +1,30 @@
+// let numbers = [10, 20, 30, 40, 50, 60];
+// console.log(numbers[0]);
+// console.log(numbers[1]);
+// console.log(numbers[2]);
+// console.log(numbers[3]);
+// console.log(numbers[4]);
+// console.log(numbers[5]);
+
+
+
+// array desturcture
+
+// let numbers = [10, 20, 30, 40, 50, 60];
+// let [num1, num2, num3, num4, num5, num6] = numbers;
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console.log(num4);
+// console.log(num5);
+// console.log(num6);
+
+
+
+// spreed oparetor
+
+// let numbers = [12, 20, 30, 40, 50, 60];
+// let [num1, num2, ...z] = numbers;
+// console.log(num1);
+// console.log(num2);
+// console.log(z);
