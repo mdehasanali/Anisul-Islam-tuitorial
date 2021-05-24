e553989788562c15adbfbdf86f78891bd3af172c
Md. Ehasan Ali <mdehasanali100@gmail.com>
2021-05-24 08:47:26 +0600

    comment added

--- 38 Destructuring array and objects/nested Object Desturcture.js
+++ 38 Destructuring array and objects/nested Object Desturcture.js
@@ -1,27 +1,27 @@
-// nested object desturcture
+// // nested object desturcture
 
-const studentInfo = {
-    id: 101,
-    fullName: 'Md. Ehasan Ali',
-    age: 18,
-    cgpa: 2.81,
-    language: {
-        native: 'Bangla',
-        beginner: 'English'
-    }
-};
+// const studentInfo = {
+//     id: 101,
+//     fullName: 'Md. Ehasan Ali',
+//     age: 18,
+//     cgpa: 2.81,
+//     language: {
+//         native: 'Bangla',
+//         beginner: 'English'
+//     }
+// };
 
-const {
-    id,
-    fullName,
-    age,
-    cgpa,
-    language
-} = studentInfo;
+// const {
+//     id,
+//     fullName,
+//     age,
+//     cgpa,
+//     language
+// } = studentInfo;
 
-console.log(id);
-console.log(fullName);
-// console.log(studentInfo.language.native);
+// console.log(id);
+// console.log(fullName);
+// // console.log(studentInfo.language.native);
 
-console.log(language.native);
-console.log(language.beginner);
+// console.log(language.native);
+// console.log(language.beginner);
--- 38 Destructuring array and objects/object Destructure.js
+++ 38 Destructuring array and objects/object Destructure.js
@@ -12,21 +12,21 @@
 
 
 
-const studentInfo = {
-    id: 101,
-    fullName: 'Md. Ehasan Ali',
-    age: 18,
-    cgpa: 2.81
-};
+// const studentInfo = {
+//     id: 101,
+//     fullName: 'Md. Ehasan Ali',
+//     age: 18,
+//     cgpa: 2.81
+// };
 
-const {
-    id,
-    fullName,
-    age,
-    cgpa
-} = studentInfo;
+// const {
+//     id,
+//     fullName,
+//     age,
+//     cgpa
+// } = studentInfo;
 
-console.log(id);
-console.log(fullName);
-console.log(age);
-console.log(cgpa);
+// console.log(id);
+// console.log(fullName);
+// console.log(age);
+// console.log(cgpa);
